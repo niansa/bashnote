5432d6fe6b0ea190e239e2de6e755b06238e28a2
niansa <anton-sa@web.de>
2016-11-06 16:26:47 +0100

    Update bashnotes.sh

--- bashnotes.sh
+++ bashnotes.sh
@@ -4,3 +4,16 @@ echo $welcomemsg
 echo "For command-list, type cmds."
 echo -n '$ '
 read cmd
+if [ $cmd = "cmds" ]
+then
+ echo "command-list: cmds, list, add, del"
+fi
+if [ $cmd = "list" ]
+then
+ ls ~/${dir}/
+if
+if [ $cmd = "add" ]
+then
+ echo -n "Name of new note: "
+ read namey
+ echo "" > ~/${dir}/${namey}
